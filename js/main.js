d0719b4c0821cad169d5b9b84798420d486f56c3
johnnylin1113 <43366316+johnnylin1113@users.noreply.github.com>
2025-06-02 17:58:26 +0800

    Optimize md template format

--- js/main.js
+++ js/main.js
@@ -42,18 +42,19 @@ _You **can** combine them_
 
 * Item 1
 * Item 2
-* Item 2a
-* Item 2b
-* Item 3a
-* Item 3b
+    * Item 2a
+    * Item 2b
+* Item 3
+    * Item 3a
+    * Item 3b
 
 ### Ordered
 
 1. Item 1
 2. Item 2
 3. Item 3
-1. Item 3a
-2. Item 3b
+    1. Item 3a
+    2. Item 3b
 
 ## Images
 
